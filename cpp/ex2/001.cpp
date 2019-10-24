3cf8c641a5ddb3475519bf93234ec3de3affbcb4
cpplay <2641797006@qq.com>
2019-10-24 16:17:36 +0800

    -

--- cpp/ex2/001.cpp
+++ cpp/ex2/001.cpp
@@ -3,20 +3,39 @@
 #include <string>
 #include <stdarg.h>
 #include "des.h"
+#include "base64.h"
 using namespace std;
 using namespace akm;
 
-const string oe="-e", od="-d";
+const string oe="-e", od="-d", oeb="-eb", odb="-db";
 void kerr(const char*, ...);
 
 int main(int argc, char **argv)
 {
-	if (argc < 4 || ((oe!=argv[1]) && (od!=argv[1]))) {
-		cerr<<"Usage: "<<argv[0]<<" -e/-d [in_file] [out_file]"<<endl;
-		return 0;
-	}
 	Des des;
 	string s;
+
+if (argc == 2 && (argv[1]==oeb || argv[1]==odb)) {
+
+	string s1;
+	Base64 base64;
+
+	cout<< (argv[1] == oeb ? "[Encrypt]" : "[Decrypt]") <<'\n';
+	cout<< "please enter a string: \n";
+	getline(cin, s);
+
+	cout<< "\nplease enter the Password: ";
+	getline(cin, s1);
+	des.set_key(s1);
+
+	if (argv[1] == oeb)
+		cout<< base64( des.encrypt(s) ) <<endl;
+	else
+		cout<< des.decrypt( base64[s] ) <<endl;
+	return 1;
+}
+
+if (argc == 4 && (argv[1]==oe || argv[1]==od)) {
 
 	fstream in(argv[2], ios::binary | ios::in);
 	if ( ! in.is_open() )
@@ -34,6 +53,12 @@ int main(int argc, char **argv)
 		des.encrypt(in, out);
 	else
 		des.decrypt(in, out);
+	return 1;
+}
+
+	cerr<<"Usage: "<<argv[0]<<" -e/-d [in_file] [out_file]"<<endl;
+	cerr<<"Usage: "<<argv[0]<<" -eb/-db"<<endl;
+	return 0;
 }
 
 void
